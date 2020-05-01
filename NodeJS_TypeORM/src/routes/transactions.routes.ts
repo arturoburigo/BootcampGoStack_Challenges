b1b41a42d047d2c57cfc48134b4e023f0adb00d9
Kawaxzx <burigoarturo3@gmail.com>
2020-05-01 19:20:13 -0300

    Postgres,TypeORM,Multer

---
+++ NodeJS_TypeORM/src/routes/transactions.routes.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import multer from 'multer';
+
+import UploadConfig from '../config/upload';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+
+const upload = multer(UploadConfig);
+
+const transactionsRouter = Router();
+
+transactionsRouter.get('/', async (request, response) => {
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
+});
+
+transactionsRouter.post('/', async (request, response) => {
+  const { title, value, type, category } = request.body;
+
+  const createTranscation = new CreateTransactionService();
+
+  const transaction = await createTranscation.execute({
+    title,
+    value,
+    type,
+    category,
+  });
+
+  return response.json(transaction);
+});
+
+transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send('Transaction Deleted');
+});
+
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const imporTransactions = new ImportTransactionsService();
+
+    const transaction = await imporTransactions.execute(request.file.path);
+
+    return response.json(transaction);
+  },
+);
+
+export default transactionsRouter;
